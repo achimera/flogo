53cfc15766814c85b89b4b767db7f591112a3922
Alessandro Chimera <achimera@Alessandros-MBP.fritz.box>
2019-03-22 16:20:42 +0100

    info

--- activity/raspicamera/activity.go
+++ activity/raspicamera/activity.go
@@ -59,7 +59,8 @@ func (a *RaspicameraActivity) Eval(context activity.Context) (done bool, err err
 	// Create a client for raspicam.
 	still := raspicam.NewStill()
 	preview := still.Preview
-	preview.Mode = raspicam.PreviewMode(2)
+	
+	log.Info("Preview Mode %v", preview.Mode)
 
 	still.Preview = preview
 
